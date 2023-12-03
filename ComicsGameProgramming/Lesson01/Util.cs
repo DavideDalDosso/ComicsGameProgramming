a25be1b7f71ae91044b486d6cc83b9f568d0440a
DavideDalDosso <DavideDalDosso08@gmail.com>
2023-12-03 23:00:20 +0100

    Setting up ex03 + util

---
+++ ComicsGameProgramming/Lesson01/Util.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Util
+{
+    public static int ReadInt(Action wrongFormatAction)
+    {
+        int number = 0;
+        bool invalid;
+        do
+        {
+            invalid = false;
+            string input = Console.ReadLine();//Reading input
+            if (input == null) input = "";//Resolving null exception for compiler sake
+
+            try
+            {
+                number = int.Parse(input);//Converting input to number
+            }
+            catch (Exception ex)
+            {
+                invalid = true;//Uh oh, no number can be read
+                wrongFormatAction.Invoke();
+            }
+
+
+        } while (invalid);
+
+        return number;
+    }
+}
--- ComicsGameProgramming/Lesson01/ex02/Program02.cs
+++ ComicsGameProgramming/Lesson01/ex02/Program02.cs
@@ -4,24 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Program01
+class Program02
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Insert the length: ");//Prompt
 
         int length = 0;
+        int height = 0;
         bool invalid;
         do
         {
-            invalid = false;//Resetting flag in orded to possibly leave loop
+            invalid = false;
             string input = Console.ReadLine();//Reading input
             if (input == null) input = "";//Resolving null exception for compiler sake
 
             try
             {
                 length = int.Parse(input);//Converting input to number
-            } catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 invalid = true;//Uh oh, no number can be read
             }
@@ -29,19 +31,46 @@ public class Program01
 
         } while (invalid);
 
+        Console.WriteLine("Insert the height: ");//Prompt
+
+        do//Same deal but for a different variable
+        {
+
+            invalid = false;
+            string input = Console.ReadLine();
+            if (input == null) input = "";
+
+            try
+            {
+                height = int.Parse(input);
+            }
+            catch (Exception ex)
+            {
+                invalid = true;
+            }
+
+
+        } while (invalid);//We can recycle the previous flag for this loop too
+
         string buffer = "";//Just a string buffer to avoid clogging the output stream for every single character
         string buffer2 = "";
 
         Console.WriteLine("\n");
 
-        for (int i=0; i < length; i++)
+        for (int i = 0; i < length; i++)
         {
-            
-            buffer += '*';//Put one more character to the buffer for the iteration
-            buffer2 += buffer+'\n';
+            buffer += "*";
+        }
 
+        buffer += "\n";
+
+        for (int i = 0; i < height; i++)
+        {
+            buffer2 += buffer;
         }
 
         Console.WriteLine(buffer2);//Output of this iteration
     }
+
+
 }
