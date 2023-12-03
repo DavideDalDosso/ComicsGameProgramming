4007904ecf3df860704c915786b51605fa590731
DavideDalDosso <DavideDalDosso08@gmail.com>
2023-12-03 19:02:06 +0100

    Lesson01 ex00

---
+++ ComicsGameProgramming/Lesson01/ex00/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static void Main()
+    {
+        //Instancing our custom class alongside the password
+        PasswordLock passwordLock = new PasswordLock("Our secret 1337");
+
+        Console.WriteLine("Please insert the password: ");//Prompt
+        bool invalid;//Our flag for the DO WHILE loop
+        do
+        {
+            string input;//The buffer for input
+
+            input = Console.ReadLine();//Reading input
+            if (input == null) input = "";//Resolving null exception for compiler sake
+
+            invalid = !passwordLock.CheckCorrect(input);//If it's wrong, mark for another attempt
+
+            if(invalid) Console.WriteLine("Unfortunately the password is wrong. Please retry... "); //Wrong answer message
+
+        } while (invalid);//Retry the procedure if it's wrong
+
+        Console.WriteLine("Congratulations, the password is correct. ");
+    }
+
+    public class PasswordLock
+    {
+        private string password;
+        public PasswordLock(string password)
+        {
+            this.password = password;
+        }
+        public bool CheckCorrect(string password)//Check if the password equals the parameter
+        {
+            return password.Equals(this.password);
+        }
+    }
+
+}
